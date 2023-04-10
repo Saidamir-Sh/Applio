684a22aab99b2fcbadc9ccc178cdd328500e7329
Saidamir <saidamirpl@gmail.com>
2023-04-10 16:48:52 +0200

    custom user manager

--- backend-applio/applio/Users/models.py
+++ backend-applio/applio/Users/models.py
@@ -2,16 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 class CustomUserManager(BaseUserManager):
+   def create_user(self, username, email, password, **extra_fields):
+      if not username:
+         raise ValueError('Username is required')
+      email = self.normalize_email(email)
+      user = self.model(username=username, email=email, **extra_fields)
+      user.set_password(password)
+      user.save()
+      return user
     
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username: models.CharField(db_index=True, max_length=240, unique=True)
-    email: models.EmailField(max_length=55, unique=True)
-    password: models.CharField(max_length=8)
+    username = models.CharField(db_index=True, max_length=240, unique=True)
+    email = models.EmailField(max_length=55, unique=True)
+    password = models.CharField(max_length=8)
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['username', 'email', 'password']
+    REQUIRED_FIELDS = ['email']
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+       return self.email
 
     class Meta:
       verbose_name = 'User'
